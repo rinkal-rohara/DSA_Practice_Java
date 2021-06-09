f20949fbc9cdaff2b54ce4230152fc2bc89af866
Rinkal Rohara <28861958+rinkal-rohara@users.noreply.github.com>
2021-06-09 22:43:34 +0530

    Problem Partition012: Sort an array of 0s, 1s and 2s.

---
+++ com/dsa/sorting/problems/Partition012.java
@@ -0,0 +1,56 @@
+/* 
+    Given an array containing 0s, 1s and 2s. 
+    Write an algorithm to sort array so that 0s come first followed by 1s and then 2s in the end. 
+*/
+package com.dsa.sorting.problems;
+
+public class Partition012{
+
+    public static void main(String args[]){
+        int inputArr[] = {1, 0, 0, 2, 1, 1, 0, 2, 1, 1, 1, 0, 1, 2, 0};
+        printArray(inputArr);
+
+        sort(inputArr);
+
+        printArray(inputArr);
+        
+
+    }
+
+
+    // Sorting in ascending order
+    static void sort(int arrayToSort[]){
+        int last0Index = -1;
+        int first2Index = arrayToSort.length;
+        int pointer = last0Index + 1;
+
+        while(pointer < first2Index){
+            if(arrayToSort[pointer] == 0){
+                last0Index++;
+                int temp = arrayToSort[pointer];
+                arrayToSort[pointer] = arrayToSort[last0Index];
+                arrayToSort[last0Index] = temp;
+            }else if(arrayToSort[pointer] == 2){
+                first2Index--;
+                int temp = arrayToSort[pointer];
+                arrayToSort[pointer] = arrayToSort[first2Index];
+                arrayToSort[first2Index] = temp;
+            }else{
+                pointer++;
+            }
+            
+        }
+    }
+
+    static void printArray(int arrayToPrint[]){
+
+        System.out.print("| ");
+
+        for(int element : arrayToPrint){
+            System.out.print(element + " | ");
+        }
+        System.out.println();
+
+    }
+
+}
