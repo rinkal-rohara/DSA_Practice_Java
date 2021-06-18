49a3d045a92ec60a1a233804a3190def68b03850
Rinkal Rohara <28861958+rinkal-rohara@users.noreply.github.com>
2021-06-18 23:55:30 +0530

    Array Reduction: Prints number of non zero elements left after each reduction

---
+++ com/dsa/sorting/problems/ArrayReduction.java
@@ -0,0 +1,27 @@
+/* 
+Given an array of positive elements. You need to perform reduction operation. 
+In each reduction operation smallest positive element value is picked and all the elements are substracted by that value.
+You need to print the number of elements left after each reduction process.*/
+package com.dsa.sorting.problems;
+import java.util.Arrays;
+
+public class ArrayReduction {
+    public static void main(String...args){
+        int inputArr[] = {5, 1, 1, 1, 2, 3, 5};
+        // expected output: 4 3 2 0
+        reduceAndPrint(inputArr);
+    }
+
+    private static void reduceAndPrint(int[] inputArr){
+        Arrays.sort(inputArr);
+
+        int reducer = inputArr[0];
+        for(int i = 0; i < inputArr.length; i++){
+            if(inputArr[i] - reducer > 0){
+                System.out.println(inputArr.length - i);
+                reducer = inputArr[i];
+            }
+        }
+    }
+
+}
